fac44d5f8b4fcfe0ddc1cfffdba8a0f21d08efa7
Pepe <webpedrod@gmail.com>
2022-03-07 16:42:22 -0300

    Feat: melhoras no código e console.log

--- desafio.js
+++ desafio.js
@@ -1,27 +1,27 @@
 const codProduto = []
-
-for (let i = 0; codProduto.length <= 9; i++) {
+for (let i = 1; codProduto.length <= 10; i++) {
     codProduto.push(i)
 }
-
 console.log (codProduto);
 
 let produtos = ['banana', 'maçã', 'uva', 'pêra', 'tangerina', 'melancia', 'morango', 'melão', 'mamão', 'kiwi']
 console.log(produtos);
 
-let preco = ['R$ 4,00','R$ 4,00','R$ 8,00','R$ 5,00','R$ 8,00','R$ 4,00','R$ 5,00','R$ 4,00','R$ 6,00','R$ 10,00']
+let preco = [4, 4, 8, 5, 8, 4, 5, 4, 6, 10]
 console.log(preco);
 
-//var precoBRL = preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}); -> pensei em colocar os valores em BRL, mas mantive assim por agora.
+var precoBRL = preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+console.log(precoBRL)
 
 const codProdutoPreco = []
 
 for (let i=0; codProdutoPreco.length < produtos.length; i++) {
     codProdutoPreco.push(
         {
-            codigo: codProduto[i],
+            codigo: i<9 ? "00"+ codProduto[i] : "0" + codProduto[i],
             produto: produtos[i],
-            precoProduto: preco[i],
+            precoProduto: preco[i].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}),
         }
     )
 }
